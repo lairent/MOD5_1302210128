10c6629d2754d7c0dfedc075db4789721a94d7b2
PRAKTIKAN <muhammadzulfadly@student.telkomuniversity.ac.id>
2023-03-24 08:05:01 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace modul5_1302210128
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class penjumlahan
+    {
+        
+    }
+}
